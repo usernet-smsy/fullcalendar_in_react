adf14a00e393d3297756dbc109bbb1ac84df920a
usernet-smsy <usernetsmsy@gmail.com>
2022-01-22 17:03:49 +0500

    first-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from "@fullcalendar/interaction"
+import listPlugin from '@fullcalendar/list';
 
 function App() {
+
+  const [events, setEvents] = useState<Array<any>>([]);
+
+  const handleDateClick = (arg: any) => {
+    setEvents((prevState) => ([...prevState, { title: "hiyya", date: arg.dateStr }]))
+    console.log('arg -> ', arg)
+  }
+
+  function renderEventContent(eventInfo: any) {
+    console.log("event info -> ", eventInfo);
+    eventInfo.backgroundColor = "grey";
+    eventInfo.isDraggable = true;
+    eventInfo.isDragging = true;
+    return (
+      <div className='date_box'>
+        <button>{eventInfo.timeText}</button>
+        <i>{eventInfo.event.title}</i>
+      </div>
+    )
+  }
+
+  const injectCellContent = (args: any) => {
+    return (
+      <div>
+        <button>{args.dayNumberText}</button>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin, listPlugin]}
+        initialView="dayGridMonth"
+        dateClick={handleDateClick}
+        events={events}
+        eventContent={renderEventContent}
+        dayCellContent={injectCellContent}
+        contentHeight={600}
+        slotLabelContent={"asdfas"}
+        aspectRatio={0.8}
+      />
     </div>
   );
 }
